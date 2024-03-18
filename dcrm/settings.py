695757aa98eab0a25476af1d769bb5ff8952efb4
Suhail Ahamed <suhailahamedd27@gmail.com>
2024-03-18 22:42:56 +0530

    set alowed host

--- dcrm/settings.py
+++ dcrm/settings.py
@@ -11,9 +11,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jcxge@8cwms-4-$&qia^6p+^8-qwrsw7vey#0e6e326apg3mvo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
