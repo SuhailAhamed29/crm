725af477b0df475023678d215c547c7965f122a9
Suhail Ahamed <suhailahamedd27@gmail.com>
2024-03-19 12:37:04 +0530

    all done

--- dcrm/urls.py
+++ dcrm/urls.py
@@ -4,4 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('website.urls')),
+    #  path('', include('website/templates/home.html')),
+
 ]
